92e29186dbb2c14497f18f232b2f9e6a85a4c553
soloexcel <solomonlekan1@gmail.com>
2023-01-23 00:29:48 -0500

    reconfigured the image src hostname

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ipfs.infura.io'],
+    // domains: ['ipfs.infura.io'],
+    domains: ['prowess.infura-ipfs.io'],
   },
 };
 
